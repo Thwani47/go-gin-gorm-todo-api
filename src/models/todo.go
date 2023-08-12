61662d0795f732316bf33fb913443c384b307b97
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-08-12 22:07:39 +0200

    added a beforeCreate todo hook to ensure todo title isn't empty

--- src/models/todo.go
+++ src/models/todo.go
@@ -1,9 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Todo struct {
 	gorm.Model
 	Title string `gorm:"not null" json:"title"`
 	Done  bool   `gorm:"default:false"`
+}
+
+func (todo *Todo) BeforeCreate(tx *gorm.DB) (err error) {
+	if todo.Title == "" {
+		err = errors.New("Title is required")
+	}
+	return
 }
